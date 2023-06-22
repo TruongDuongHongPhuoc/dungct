dee3d193ba30d60f371b9f346639d3abf5d145b5
TruongDuongHongPhuoc <121841902+TruongDuongHongPhuoc@users.noreply.github.com>
2023-06-22 12:54:41 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
- $conn = mysqli_connect('localhost', 'root', '', 'Dungct')
+ $conn = mysqli_connect('us-cdbr-east-06.cleardb.net', 'b8e893b7bb74ae', 'cd748e34', 'heroku_588b93252fb9b95')
  or die("Cannot connect to the database: " . mysqli_connect_error());
 
 ?>
